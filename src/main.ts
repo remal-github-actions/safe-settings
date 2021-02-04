4fe17ad58fde785e2f9512d6e42c00c4739a1a0c
Semyon Levin <levin.semen@gmail.com>
2021-02-04 23:00:41 +0300

    test

--- src/main.ts
+++ src/main.ts
@@ -23,7 +23,7 @@ async function run(): Promise<void> {
                 repo: context.repo.repo,
                 path: configPath,
                 ref: settingsRef !== '' ? settingsRef : undefined,
-            })
+            }).catch(reason => typeof reason)
         ))
         for (const foundContent of foundContents) {
             core.info(JSON.stringify(foundContent))
