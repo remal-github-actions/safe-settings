aff587ce0c4174478e280f59ad658143c9ebd772
Semyon Levin <levin.semen@gmail.com>
2021-02-04 22:13:47 +0300

    test

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,33 @@
 import * as core from '@actions/core'
+import {newOctokitInstance} from './internal/octokit'
+import {context} from '@actions/github'
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+const configPaths = ['json', 'json5', 'yaml', 'yml'].map(ext => `.github/safe-settings.${ext}`)
+
+const githubToken = core.getInput('githubToken', {required: true})
+core.setSecret(githubToken)
+
+const settingsRef = core.getInput('settingsRef')
+
+const octokit = newOctokitInstance(githubToken)
 
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
 async function run(): Promise<void> {
     try {
-        core.info("action logic")
+        const foundContents = await Promise.all(configPaths.map(configPath =>
+            octokit.repos.getContent({
+                owner: context.repo.owner,
+                repo: context.repo.repo,
+                path: configPath,
+                ref: settingsRef !== '' ? settingsRef : undefined,
+            })
+        ))
+        for (const foundContent of foundContents) {
+            core.info(JSON.stringify(foundContent))
+        }
 
     } catch (error) {
         core.setFailed(error)
@@ -13,3 +36,5 @@ async function run(): Promise<void> {
 
 //noinspection JSIgnoredPromiseFromCall
 run()
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
